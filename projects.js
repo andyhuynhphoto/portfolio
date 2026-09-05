58d9a27f9268a1b7f51edcfdf1a1e0ecff1ece34
Andy Tac - Photographer / Videographer / Content Creator <andyhuynh1108@gmail.com>
2026-09-05 19:21:08 -0400

    Add portraitProjects object with studio and outdoor categories

---
+++ projects.js
@@ -0,0 +1,55 @@
+const portraitProjects = {
+
+  studio: [
+    {
+      title: "Women's Portraits",
+      slug: "womens-portraits",
+      cover: "images/portrait/studio/womens-portraits/cover.jpeg"
+    },
+
+    {
+      title: "Men's Portraits",
+      slug: "mens-portraits",
+      cover: "images/portrait/studio/mens-portraits/cover.jpeg"
+    },
+
+    {
+      title: "Birthday Portraits",
+      slug: "birthday-portraits",
+      cover: "images/portrait/studio/birthday-portraits/cover.jpeg"
+    },
+
+    {
+      title: "Baby Portraits",
+      slug: "baby-portraits",
+      cover: "images/portrait/studio/baby-portraits/cover.jpeg"
+    }
+  ],
+
+  outdoor: [
+    {
+      title: "Hong Kong Style",
+      slug: "hong-kong-style",
+      cover: "images/portrait/outdoor/hong-kong-style/cover.jpeg"
+    },
+
+    {
+      title: "Daylight Streets",
+      slug: "daylight-streets",
+      cover: "images/portrait/outdoor/daylight-streets/cover.jpeg"
+    },
+
+    {
+      title: "Night Streets",
+      slug: "night-streets",
+      cover: "images/portrait/outdoor/night-streets/cover.jpeg"
+    },
+
+    {
+      title: "Subway After Dark",
+      slug: "subway-after-dark",
+      cover: "images/portrait/outdoor/subway-after-dark/cover.jpeg"
+    }
+  ]
+
+};
